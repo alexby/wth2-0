4cc6c95c10bafca7f8a49707b80f97c7fec1d007
ilyabasiuk <ilyabasiuk@gmail.com>
2015-03-28 03:34:43 +0300

    resored suppoting socket io

--- public/js/connector.js
+++ public/js/connector.js
@@ -1,21 +1,20 @@
 /**
  * Created by Asus on 3/27/2015.
  */
-define(
-    'connector',
-    ["io"],
-    function(io){
-        var socket = io();
+var connector =  function(){
+    var socket = io();
 
-        return {
-            send: function (message) {
-                socket.emit('chat message', message);
-            },
-            onReceive: function () {
-                socket.on('chat message', function(msg){
-                    $('#messages').append($('<li>').text(msg));
-                });
-            }
+    return {
+        send: function (message) {
+            socket.emit('chat message', message);
+        },
+        onReceive: function () {
+            socket.on('chat message', function(msg){
+                $('#messages').append($('<li>').text(msg));
+            });
+        },
+        sendImage:  function() {
+
         }
     }
-);
+}();
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,20 @@
 /**
  * Created by Asus on 3/27/2015.
  */
+
+var connector;
+
 var app = function(){
-	return {init:function() {
-        var socket = io();
-        $('form').submit(function(){
-            socket.emit('chat message', $('#m').val());
-            $('#m').val('');
-            return false;
-        });
-        socket.on('chat message', function(msg){
-            $('#messages').append($('<li>').text(msg));
-        });
-	}}
-	
+	return {
+        init:function() {
+            $('form').submit(function(){
+                connector.send($('#m').val());
+                $('#m').val('');
+                return false;
+            });
+            connector.onReceive(function(msg){
+                $('#messages').append($('<li>').text(msg));
+            });
+	    }};
 }()
 
